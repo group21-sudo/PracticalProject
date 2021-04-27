bfb040723a9164d06ea8b89ee4778220ff8e0de9
211806153 <1511080749@qq.com>
2021-04-27 19:45:07 +0800

    fuzzy search function

--- src/com/group21/www/dao/BookInfoDao.java
+++ src/com/group21/www/dao/BookInfoDao.java
@@ -20,6 +20,18 @@ public class BookInfoDao {
         return false;
     }
     public List<BookInfo> fuzzySearch(String key){
-        return null;
+        ArrayList<BookInfo> bookInfos = new ArrayList<>();
+        String queryStr = "select * from bookinfo where bookname like '%"+key+"%'";
+        Connection connection = DBConnection.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(queryStr);
+            while (resultSet.next()){
+                bookInfos.add(new BookInfo(resultSet.getInt("id"), resultSet.getString("bookName"), resultSet.getDouble("price"), resultSet.getString("nick")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return bookInfos;
     }
 }
