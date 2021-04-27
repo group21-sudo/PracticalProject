5690fdd6000556675e7951aa177fae01c129515c
cychenye <770403211@qq.com>
2021-04-27 19:54:54 +0800

    motify get product information

--- tb.py
+++ tb.py
@@ -1,4 +1,4 @@
-import requests
+﻿import requests
 import re
 
 headers = {
@@ -17,17 +17,32 @@ def get_html(url):
 
 
 def get_data(html, goodlist):
-    pass
-
+    tlist = re.findall(r'\"raw_title\"\:\".*?\"', html)
+    plist = re.findall(r'\"view_price\"\:\"[\d\.]*\"', html)
+    nlist = re.findall(r'\"nick\"\:\".*?\"', html)
+    for i in range(len(tlist)):
+        title = eval(tlist[i].split(':')[1]) 
+        price = eval(plist[i].split(':')[1])
+        name = eval(nlist[i].split(':')[1])
+        goodlist.append([title, price, name])
 
 def write_data(list, num):
-    pass
+    with open("D:\list.txt", "w", encoding="utf-8") as f:
+        for line in list:
+            for item in line:
+                f.write(item + "###")
+            f.write("\n")
 
 
 def main():
     goods = '编程实践'
     url = 'https://s.taobao.com/search?q=' + goods + '&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20210427&ie=utf8&sort=sale-desc'
+    infoList = []
     html = get_html(url)
+    get_data(html, infoList)
+    print(len(infoList))
+    print(infoList)
+    write_data(infoList, len(infoList))
 
 if __name__ == '__main__':
     main()
