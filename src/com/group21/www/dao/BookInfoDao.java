ed6846820d5162b5cbcf45e3d6a26204372f6baf
211806205chenqi <2056898264@qq.com>
2021-04-27 20:17:02 +0800

    modify the code for add data to database

--- src/com/group21/www/dao/BookInfoDao.java
+++ src/com/group21/www/dao/BookInfoDao.java
@@ -10,7 +10,40 @@ import java.util.List;
 
 public class BookInfoDao {
     public boolean addListFromFile(String pathname) {
-        return false;
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement preparedStatement = null;
+        BufferedReader bufferedReader = null;
+        boolean flag = true;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(new File(pathname)));
+            String line;
+            String[] infos;
+            while(1) {
+                line = bufferedReader.readLine();
+                infos = line.split("###");
+                String addInfoStr = "insert into bookinfo(bookName,price,nick) values(?,?,?)";
+                preparedStatement = connection.prepareStatement(addInfoStr);
+                preparedStatement.setString(1, infos[0]);
+                preparedStatement.setDouble(2, Double.valueOf(infos[1]));
+                preparedStatement.setString(3, infos[2]);
+                preparedStatement.executeUpdate();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return flag;
     }
 
     public List<BookInfo> getTotalBookInfo(){
